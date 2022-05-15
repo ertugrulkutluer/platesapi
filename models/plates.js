858abad5c17d84d40cd51a03f8dbaa46e64b2d77
Ertugrul Kutluer <ertugrul@finedinemenu.com>
2022-05-15 16:07:10 +0300

    fix: minor

--- models/plates.js
+++ models/plates.js
@@ -1,5 +1,4 @@
 let plates = require("../data/plates.json")
-const { v4: uuidv4 } = require('uuid')
 
 module.exports.findAll = () => {
     return new Promise((resolve, reject) => {
@@ -13,35 +12,3 @@ module.exports.findById = (id) => {
         resolve(plate)
     })
 }
-
-// function create(product) {
-//     return new Promise((resolve, reject) => {
-//         const newProduct = {id: uuidv4(), ...product}
-//         products.push(newProduct)
-//         if (process.env.NODE_ENV !== 'test') {
-//             writeDataToFile('./data/products.json', products);
-//         }
-//         resolve(newProduct)
-//     })
-// }
-
-// function update(id, product) {
-//     return new Promise((resolve, reject) => {
-//         const index = products.findIndex((p) => p.id === id)
-//         products[index] = {id, ...product}
-//         if (process.env.NODE_ENV !== 'test') {
-//             writeDataToFile('./data/products.json', products);
-//         }
-//         resolve(products[index])
-//     })
-// }
-
-// function remove(id) {
-//     return new Promise((resolve, reject) => {
-//         products = products.filter((p) => p.id !== id)
-//         if (process.env.NODE_ENV !== 'test') {
-//             writeDataToFile('./data/products.json', products);
-//         }
-//         resolve()
-//     })
-// }
